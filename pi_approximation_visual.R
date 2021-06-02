8e2828516f29fecb59540faef375b815a2a12142
Ahmad Qadri <arqchicago@gmail.com>
2021-06-01 22:26:43 -0400

    used ggplot2 to show simulation setup

--- pi_approximation_visual.R
+++ pi_approximation_visual.R
@@ -1,12 +1,14 @@
 require(plotrix)
 require(grid)
+require(ggplot2)
 
 # reading data from the data file
 setwd('C:/Users/Ahmad/Desktop/Ahmad/python/pi_approximation_visual')
-pi_data <- read.csv('./data/data.csv', row.names=1)
+pi_data <- read.csv('./data/data.csv', row.names=1, header=TRUE)
 pi_data['pi_appx'][is.na(pi_data['pi_appx'])] <- 0
+pi_data$iter <- rownames(pi_data)
+pi_data$iter <- as.numeric(pi_data$iter)
 pi_data
-
 
 # create the visual of a circle of radius 1 inscribed inside of a square of side
 # 2, both centered at the origin
@@ -25,4 +27,25 @@ for (row in 1:nrow(pi_data)) {
   else {
     points(x, y, col = "blue", lwd = 2 )
   }
+  
 }
+
+
+#=============================================================
+require(ggplot2)
+
+getCircle <- function(center = c(0,0), diameter = 1, npoints = 100){
+  seq <- seq(0, 2*pi, length.out = npoints)
+  x_seq <- center[1] + diameter / 2 * cos(seq)
+  y_seq <- center[2] + diameter / 2 * sin(seq)
+  return(data.frame(x = x_seq, y = y_seq))
+}
+
+
+circle_data <- getCircle(diameter=2)
+ggplot(circle_data, aes(x, y)) + geom_path(size=1, col='maroon') +
+  geom_rect(xmin=-1, xmax=1, ymin=-1, ymax=1, size=1, col='darkblue', alpha=0)
+
+pi_data2 <- pi_data[1:2000,]
+ggplot(pi_data2, aes(iter, pi_appx)) + geom_point() + 
+  geom_hline(yintercept=3.14159, col='red')
