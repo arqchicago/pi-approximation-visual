2557f53282f65ee9d8f62a3a822b7998d14bd1bc
Ahmad Qadri <arqchicago@gmail.com>
2021-06-02 23:24:18 -0400

    modified plots

--- pi_approximation_visual.R
+++ pi_approximation_visual.R
@@ -2,6 +2,9 @@ require(plotrix)
 require(grid)
 require(ggplot2)
 
+#=========================================================================================================
+# read data
+
 # reading data from the data file
 setwd('C:/Users/Ahmad/Desktop/Ahmad/python/pi_approximation_visual')
 pi_data <- read.csv('./data/data.csv', row.names=1, header=TRUE)
@@ -10,30 +13,10 @@ pi_data$iter <- rownames(pi_data)
 pi_data$iter <- as.numeric(pi_data$iter)
 pi_data
 
-# create the visual of a circle of radius 1 inscribed inside of a square of side
-# 2, both centered at the origin
-plot(c(-1, 1), c(-1,1), type = "n", asp=1)
-rect( -1., -1., 1., 1., border = "blue", lwd = 2)
-draw.circle( 0, 0, 1., border = "red", lwd = 2 )
+#=========================================================================================================
+# prepare visuals
 
-for (row in 1:nrow(pi_data)) {
-  x <- pi_data[row, "x"]
-  y  <- pi_data[row, "y"]
-  in_circle  <- pi_data[row, "in_circle"]
-  
-  if(in_circle==1) {
-    points(x, y, col = "red", lwd = 2 )
-  }
-  else {
-    points(x, y, col = "blue", lwd = 2 )
-  }
-  
-}
-
-
-#=============================================================
-require(ggplot2)
-
+# show simulation setup in a visual
 getCircle <- function(center = c(0,0), diameter = 1, npoints = 100){
   seq <- seq(0, 2*pi, length.out = npoints)
   x_seq <- center[1] + diameter / 2 * cos(seq)
@@ -45,7 +28,19 @@ getCircle <- function(center = c(0,0), diameter = 1, npoints = 100){
 circle_data <- getCircle(diameter=2)
 ggplot(circle_data, aes(x, y)) + geom_path(size=1, col='maroon') +
   geom_rect(xmin=-1, xmax=1, ymin=-1, ymax=1, size=1, col='darkblue', alpha=0)
+ggsave(filename="./output/image1_setup.png", plot=last_plot(), width = 15, height = 15, units='cm')
 
+
+# show simulation results in a visual
 pi_data2 <- pi_data[1:2000,]
-ggplot(pi_data2, aes(iter, pi_appx)) + geom_point() + 
-  geom_hline(yintercept=3.14159, col='red')
+ggplot(pi_data2, aes(iter, pi_appx)) + 
+  geom_line(aes(color='simulation appx'), linetype = 1, size = 1.5) + 
+  geom_hline(aes(yintercept = 3.14159, color = "exact value"), linetype = 1, size = 1) +
+  scale_colour_manual(values = c("blue", "red")) +
+  ggtitle("Monte Carlo Simulation to approximate Pi") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Iteration",
+       y = "Pi Approximation",
+       color = "Pi")
+
+ggsave(filename="./output/simulation_results.png", plot=last_plot()) #width=10, height=5, units="cm"
